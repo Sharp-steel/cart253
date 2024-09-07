87819095d670e632d0ffb0fd1980d49d6eb40dba
Sharp-steel <ethan.arm13@gmail.com>
2024-09-06 20:16:08 -0400

    Added comments and rectangle

--- topics/function-calls/function-calls/js/script.js
+++ topics/function-calls/function-calls/js/script.js
@@ -1,24 +1,27 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Ethan Armstrong
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Learning how to utilize function calls.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates a 640x480 canvas for our masterpiece
+ */
 function setup() {
-
+    // Create the canvas at a standard resolution
+    createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws a blank piece of paper on a pink background
+ */
 function draw() {
-
+    // A pink background
+    background(255, 100, 100);
+    // The blank piece of paper
+    rect(200, 80, 240, 320);
 }
