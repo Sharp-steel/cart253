60dc1927a37ef743d933eca9502493957422eae7
Sharp-steel <ethan.arm13@gmail.com>
2024-09-06 20:53:53 -0400

    Added the Canvas and Drew a Record Label

--- topics/drawing-practice/drawing-practice/js/script.js
+++ topics/drawing-practice/drawing-practice/js/script.js
@@ -12,7 +12,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    // A nice canvas square to work with
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +21,27 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    // A grey background
+    background(150, 150, 150);
 
+    // The main part of the record is red
+    push();
+    fill(255, 0, 0);
+    stroke(255, 255, 255);
+    ellipse(320, 320, 480, 480);
+    pop();
+
+    // The label on the record
+    push();
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(320, 320, 140, 140);
+    pop();
+
+    // The hole in the record
+    push();
+    fill(150, 150, 150);
+    stroke(50, 50, 50);
+    ellipse(320, 320, 20, 20);
+    pop();
 }
