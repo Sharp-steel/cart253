665d59ea49a9664b13b31b1d91de18cb776696b7
Sharp-steel <ethan.arm13@gmail.com>
2024-09-17 14:46:18 -0400

    Added Week 3 content and updated Mr. Furious with instructions on how to change a background colour over time, how to shake objects randomly

--- topics/week-2/mr-furious/mr-furious/js/script.js
+++ topics/week-2/mr-furious/mr-furious/js/script.js
@@ -21,6 +21,14 @@ let mrFurious = {
   }
 };
 
+let sky = {
+    fill: {
+      r: 160,
+      g: 180,
+      b: 200
+    }
+}
+
 /**
  * Create the canvas
  */
@@ -32,18 +40,45 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
 
+  // Change the colour of the sky
+  sky.fill.r -= 0.5;
+  sky.fill.r = constrain(sky.fill.r, 0, 255);
+  sky.fill.g -= 0.5;
+  sky.fill.g = constrain(sky.fill.g, 0, 255);
+  sky.fill.b -= 0.5;
+  sky.fill.b = constrain(sky.fill.b, 0, 255);
+  
+  // Set the colour of the background
+  background(sky.fill.r, sky.fill.g, sky.fill.b);
+
+  // Change the colour of Mr. Furious to red
   mrFurious.fill.g = mrFurious.fill.g - 0.5;
   mrFurious.fill.b = mrFurious.fill.b - 0.5;
 
   mrFurious.fill.g = constrain(mrFurious.fill.g, 0, 255);
-  mrFurious.fill.b = constrain(mrFurious.fill.b, 0, 255)
+  mrFurious.fill.b = constrain(mrFurious.fill.b, 0, 255);
+
+  // Same thing but on less lines
+
+  // mrFurious.fill.g = constrain(mrFurious.fill.g - 2, 0, 255);
+  // mrFurious.fill.b = constrain(mrFurious.fill.b - 2, 0, 255);
+
+  // Make Mr. Furious shake
+  const x = mrFurious.x + random(-5, 5);
+  const y = mrFurious.y + random(-5, 5);
+  
+
+  // Another way of doing it
+  // mrFurious.x += random(-5, 5);
+  // mrFurious.y += random(-5, 5);
+
+
   
   // Draw Mr. Furious as a coloured circle
   push();
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
-  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  ellipse(x, y, mrFurious.size);
   pop();
 }
