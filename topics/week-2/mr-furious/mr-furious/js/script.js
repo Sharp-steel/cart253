6061f70b4295c84d729eb7848d94c4d5371a10a2
Sharp-steel <ethan.arm13@gmail.com>
2024-09-17 13:16:31 -0400

    Changed the colour transition to red

--- topics/week-2/mr-furious/mr-furious/js/script.js
+++ topics/week-2/mr-furious/mr-furious/js/script.js
@@ -33,6 +33,12 @@ function setup() {
  */
 function draw() {
   background(160, 180, 200);
+
+  mrFurious.fill.g = mrFurious.fill.g - 0.5;
+  mrFurious.fill.b = mrFurious.fill.b - 0.5;
+
+  mrFurious.fill.g = constrain(mrFurious.fill.g, 0, 255);
+  mrFurious.fill.b = constrain(mrFurious.fill.b, 0, 255)
   
   // Draw Mr. Furious as a coloured circle
   push();
