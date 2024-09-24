f56e867d4a6895d8f41a7d4ad39e5b8273109749
Sharp-steel <ethan.arm13@gmail.com>
2024-09-24 15:33:03 -0400

    Created the graphic elements and made the thunderbolt appear and disappear when pressed

--- topics/art-jam/art-jam/art-jam/README.md
+++ topics/art-jam/art-jam/art-jam/README.md
@@ -1,4 +1,4 @@
-# Template p5 Project
+# Art Jam Project
 
 Ethan Armstrong
 
@@ -6,16 +6,10 @@ Ethan Armstrong
 
 ## Description
 
-This description should help the reader understand what the program is, what any controls do, and perhaps what the desired experience it.
+A sunny day turns into a devastating thunderstorm! Click on the canvas for a thunderbolt to appear.
 
 ## Credits
 
-This bit should describe what tools were used. For example:
-
 This project uses [p5.js](https://p5js.org).
 
-## Attribution
-
-This bit should attribute any assets or other elements used taken from other sources. For example:
-
-The fish images were sourced from the [Creative Commons image "Georgia Aquarium Fish"](https://search.creativecommons.org/photos/96f6f770-eac1-488c-8abb-16bee7bcc874) by Mike Johnston which is licensed with CC BY 2.0. To view a copy of this license, visit https://creativecommons.org/licenses/by/2.0/.
+This project also uses te P5 Template. (https://pippinbarr.com/cart253/templates/template-p5-project.zip).
--- topics/art-jam/art-jam/art-jam/js/script.js
+++ topics/art-jam/art-jam/art-jam/js/script.js
@@ -6,12 +6,37 @@
  */
 
 "use strict";
-
+/**
+ * Creating the cloud
+ */
 let cloud = {
     x: 250,
     y: 100,
-    size: 100,
+    size: 250,
     fill: "#ffffff"
+}
+/**
+ * Creating the thunderbolt
+ */
+let thunderbolt = {
+    x: 250,
+    y: 250,
+    size: 50,
+    width: 50,
+    height: 280,
+    //Transparent Thunderbolt
+    fill: {r: 255, g: 255, b: 0, a: 0}
+}
+/**
+ * Creating the rain
+ */
+let rain = {
+    x: 200,
+    y: 200,
+    size: 10,
+    width: 10,
+    height: 60,
+    fill: "#0000ff"
 }
 
 /**
@@ -27,22 +52,78 @@ function setup() {
  * Creating the scene
 */
 function draw() {
-    drawCloud();
+    //Grey Background
     background(150, 150, 150);
     
     // Changing the colour of the sky
-    if (mouseX < width/4){
+    if (mouseX < width){
         background(200, 200, 255);
     }
     else {
         background(150, 150, 150);
     }
+    if (mouseY < height){
+        background(200, 200, 255);
+    }
+    else {
+        background(200, 200, 255);
+    }
+    //Calling the thunderbolt
+    drawThunderbolt();
+    //Calling the cloud
+    drawCloud();
+    //Calling the rain
+    drawRain();
 }
 
+/** 
+ * Creating the function for the cloud to show up
+*/
 function drawCloud () {
     push();
     noStroke();
-    ellipse(cloud.x, cloud.y, cloud.size);
     fill(cloud.fill);
+    ellipse(cloud.x, cloud.y, cloud.size);
+    ellipse(cloud.x - cloud.size/2, cloud.y, cloud.size/1.25);
+    ellipse(cloud.x + cloud.size/2, cloud.y, cloud.size/1.25);
     pop();
+}
+
+/**
+ * Creating the function for the thunderbolt to show up
+ */
+function drawThunderbolt () {
+    push();
+    noStroke();
+    fill(thunderbolt.fill.r, thunderbolt.fill.g, thunderbolt.fill.b, thunderbolt.fill.a);
+    rect(thunderbolt.x - 25, thunderbolt.y -30, thunderbolt.size, thunderbolt.height);
+    pop();
+}
+
+/**
+ * Creating the function for the rain to show up
+ */
+function drawRain () {
+    push();
+    noStroke();
+    fill(rain.fill);
+    rect(rain.x - 50, rain.y + 200, rain.size, rain.height);
+    rect(rain.x + 215, rain.y + 150, rain.size, rain.height);
+    rect(rain.x - 20, rain.y + 40, rain.size, rain.height);
+    rect(rain.x - 120, rain.y + 120, rain.size, rain.height);
+    rect(rain.x + 160, rain.y + 20, rain.size, rain.height);
+    rect(rain.x + 125, rain.y + 175, rain.size, rain.height);
+    pop();
+}
+
+/**
+ * When the mouse is pressed, display thunderbolt
+ */
+function mousePressed () {
+    if (thunderbolt.fill.a === 0) {
+        thunderbolt.fill.a = 255;
+    }
+    else if (thunderbolt.fill.a === 255) {
+        thunderbolt.fill.a = 0;
+    }
 }
