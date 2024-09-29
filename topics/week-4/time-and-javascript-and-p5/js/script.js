241ae194e7df3b3f461156f2e103dec863d1a973
Sharp-steel <ethan.arm13@gmail.com>
2024-09-29 13:34:04 -0400

    Finished time & js & p5

--- topics/week-4/time-and-javascript-and-p5/js/script.js
+++ topics/week-4/time-and-javascript-and-p5/js/script.js
@@ -8,17 +8,23 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+const ball = {
+    x: 0,
+    y: 200,
+    size: 50
+};
 
+function setup() {
+    createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    background(0);
+    
+    ball.x += 1;
+    
+    push();
+    noStroke();
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
 }
