f62c95799ee7a3c9f595ee8a57dfd9ae237d0117
Sharp-steel <ethan.arm13@gmail.com>
2024-09-29 15:36:59 -0400

    Completed time event module

--- topics/week-4/traffic-light/js/script.js
+++ topics/week-4/traffic-light/js/script.js
@@ -23,10 +23,13 @@ const trafficLight = {
 };
 
 /**
- * Create the canvas
+ * Create the canvas and start the light cycle
  */
 function setup() {
     createCanvas(400, 400);
+    
+    // Start the traffic light cycle
+    setInterval(changeLight, trafficLight.delay);
 }
 
 /**
@@ -41,4 +44,22 @@ function draw() {
     fill(trafficLight.fill);
     ellipse(trafficLight.x, trafficLight.y, trafficLight.size);
     pop();
+}
+
+/**
+ * Change the light through the cycle
+ */
+function changeLight() {
+    // Green goes to orange
+    if (trafficLight.fill === trafficLight.fills.go) {
+        trafficLight.fill = trafficLight.fills.slow;
+    }
+    // Orange goes to red
+    else if (trafficLight.fill === trafficLight.fills.slow) {
+        trafficLight.fill = trafficLight.fills.stop;
+    }
+    // Red goes to green
+    else if (trafficLight.fill === trafficLight.fills.stop) {
+        trafficLight.fill = trafficLight.fills.go;
+    }
 }
